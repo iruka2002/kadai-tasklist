e323b4fa5aaa1daaf209eef655ff66406ec9953c
takehiro.kadota <takehiro.kadota@gmail.com>
2017-12-03 05:06:51 +0000

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
- root to: 'tasks#index'
- 
- get 'login', to: 'sessions#new'
- post 'login', to: 'sessions#create'
- delete 'logout', to: 'sessions#destroy'
- 
- get 'signup', to: 'tasks#new'
- resources :tasks, only: [:index, :show, :new, create]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
